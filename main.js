bc771031b4125666a4ed0c2605196943eb91c84c
Connor <connorwhisman17@gmail.com>
2023-03-09 22:04:21 -0500

    Allow length param. Remove post temporarily

--- main.js
+++ main.js
@@ -127,16 +127,42 @@ var data = {
 
 
 APP.get('/', function (req, res) {
+    var obj;
     res.header({'Access-Control-Allow-Origin': '*'});
-    res.send(data);
-    console.log('Recieved a GET request. Responding with: ' + JSON.stringify(data));
+
+    if (typeof req.query.length !== 'undefined' || req.query.length !== '') {
+        if (req.query.length === '*') {
+            obj = data;
+        }
+        else {
+            try {
+                var length = Number(req.query.length);
+                if (length != 0) {
+                    obj = {'containers': data.containers.slice(0, length)};
+                }
+            }
+            catch {
+                obj.data = {'containers': data.containers[0]};
+            }
+        }
+    }
+    else {
+        obj.data = {'containers': data.containers[0]};
+    }
+    res.send(obj);
+
+    console.log(req.query);
+    console.log('Recieved a GET request. Responding with: ' + JSON.stringify(obj));
 })
 
-APP.post('/', function(req, res) {
-    console.log(req.body);
-    data.obj.push(req.body);
-    res.send('recieved');
-})
+
+// APP.post('/', function(req, res) {
+//     console.log(req.body);
+//     data.obj.push(req.body);
+//     res.send('recieved');
+// })
+
+
 APP.listen(
     PORT,
     () => console.log('Listening..')
